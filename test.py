6ae27266373e19d2efeac5aea22efeafd3eb7843
KatherineLiu-holdon <55497481+KatherineLiu-holdon@users.noreply.github.com>
2021-04-14 16:28:16 +0800

    Add files via upload
    Detection Requirement
    • Detect the DGA domains from the corpus

---
+++ test.py
@@ -0,0 +1,81 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import math
+
+
+def cal_entropy(text):
+    h = 0.0
+    sum = 0
+    letter = [0] * 26
+    text = text.lower()
+    for char in text:
+        if char.isalpha():
+            letter[ord(char) - ord('a')] += 1
+            sum += 1
+    for i in range(26):
+        p = 1.0 * letter[i] / sum
+        if p > 0:
+            h += -(p * math.log(p, 2))
+    return h
+
+
+# 定义notdga为0,dga为1
+
+def deal_with_traindata(filename):
+    Domainlist = []
+    LableList = []
+    with open(filename, 'r') as f:
+
+        for line in f:
+            tokens = line.split(",")
+            length = len(tokens[0])
+            en = cal_entropy(tokens[0])
+            seg = len(tokens[0].split("."))
+            num = 0
+            for char in tokens[0]:
+                if char.isdigit():
+                    num += 1
+
+            if tokens[1] == 'notdga\n':
+                lable = 0
+            else:
+                lable = 1
+
+            Domainlist.append([length, num, en, seg])
+            LableList.append(lable)
+    return Domainlist, LableList
+
+
+def deal_with_testdata(filename):
+    Testlist = []
+    with open(filename, 'r') as f:
+        for sample in f:
+            length = len(sample)
+            seg = len(sample.split("."))
+            num = 0
+            for char in sample:
+                if char.isdigit():
+                    num += 1
+            Testlist.append([length, num, seg])
+
+    return Testlist
+
+
+featureMatrix, labelList = deal_with_traindata("train.txt")
+
+clf = RandomForestClassifier(random_state=0)
+clf.fit(featureMatrix, labelList)
+
+testMatrix = deal_with_testdata("test.txt")
+resultlist = []
+
+with open("result.txt", "w+", newline='') as f:
+    for i in testMatrix:
+        t = clf.predict([i])
+        if t == 0:
+            resultlist.append(i + ',' + 'notdga')
+        elif t == 1:
+            resultlist.append(i + ',' + 'dga')
+    for result in resultlist:
+        f.write(result+'\n')
+
